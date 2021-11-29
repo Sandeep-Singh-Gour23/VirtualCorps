5278df1fd23c88ca4f380fe3cdf2a93458a6836d
Anshika <anshikasethiya28@gmail.com>
2021-11-29 15:41:01 +0530

    create member validation fixed

--- vfrontend/src/hr/assignProject/BtnComponent.js
+++ vfrontend/src/hr/assignProject/BtnComponent.js
@@ -1,4 +1,4 @@
-import React,{useState} from "react"
+import React,{useState,useEffect} from "react"
 import Modal from "react-modal"
 import Select from "react-select"
 import './BtnComponents.style.css'
@@ -6,10 +6,35 @@ import './BtnComponents.style.css'
 
 function Button(props){
    const [modalIsOpen, setModalIsOpen] = useState(false)
-   const status =["assigned","pending","unassigned"];
+  //  const status =["assigned","pending","unassigned"];
+   const[projectManager, setProjectManager] = useState([])
 
   //fetching
-    
+    const getProjectManager = async () =>{ 
+      try {
+        const response = await fetch('/unassignedProjectManager',{
+          method : "GET",
+          headers:{
+            "Content-Type":"application/json",
+            "authorization":"bearer "+localStorage.getItem("jwt")
+          },
+        })
+        const data = await response.json();
+        console.log("Project Manager: ", data);      
+        } catch (error) {
+          console.log("Error in get project manager: ", error);
+        }  
+    }
+  
+    //posting project
+
+    // const assignProject = () => {
+    //   fetch('/assignProject')
+    // }
+
+    useEffect(() => {
+      getProjectManager()
+    }, [])
     return(
     <>
     <div className="body-bg">
@@ -23,7 +48,7 @@ function Button(props){
           </div>
       </div>
          
-         <Modal isOpen={modalIsOpen} onRequestClose={()=>setModalIsOpen(false)} shouldCloseOnOverlayClick={true} style={{overlay:{backgroundColor:'#868686'}}} >
+         <Modal isOpen={modalIsOpen} onRequestClose={()=>setModalIsOpen(false)} shouldCloseOnOverlayClick={true} ariaHideApp={false} style={{overlay:{backgroundColor:'#868686'}}} >
         <div className="box">
         <button className="closeBtn" onClick={() => setModalIsOpen(false)}>X</button>
             <h2 className="box-title">{props.title}</h2>
@@ -41,6 +66,12 @@ function Button(props){
             <Select
             className="drop-down2"
             placeholder="Select Project Manager" 
+            name="proejctManager"
+            options={projectManager.map((data) => {
+              return (
+                <option value="data.value">{data.setProjectManager}</option>
+              ) 
+            })}
             />
             <button className="button-1">Submit</button>
          </div>
